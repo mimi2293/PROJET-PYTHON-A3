e9f633cf9e0e4fd9996480dcd8f4808f76e54881
mimi2293 <myriam.khouja@edu.devinci.fr>
2026-01-08 17:45:01 +0100

    Add configuration for execution mode and paths

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+# Mode d’exécution
+MODE = "LOCAL"   # "LOCAL" ou "ANSSI"
 
+# Chemin données locales (mode LOCAL)
+BASE_PATH = r"C:\Users\khouj\Downloads\data_pour_TD_final_2026"
+
+# Flux RSS ANSSI (mode ANSSI)
+RSS_AVIS = "https://www.cert.ssi.gouv.fr/avis/feed/"
+RSS_ALERTES = "https://www.cert.ssi.gouv.fr/alerte/feed/"
